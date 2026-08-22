951845c95a1c269c4905fa34fe940c53e5ac08f7
hmpmanish <hmpmanish@gmail.com>
2026-08-22 21:01:48 +0530

    fix: add remaining core application files

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,18 @@
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col flex-1 w-full">
+      <Header />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
